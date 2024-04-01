47b9099603d8e47dade03eaa7b5f1348d5274cde
Ahmed Aly Mohamed <ahmedalimohamed1994@gmail.com>
2024-04-02 01:57:00 +0200

    function to get element with highest z-index

---
+++ highest-z-index/index.js
@@ -0,0 +1,16 @@
+function findHighestZIndex(elem) {
+  var elems = document.getElementsByTagName(elem);
+  var highest = Number.MIN_SAFE_INTEGER || -(Math.pow(2, 53) - 1);
+  for (var i = 0; i < elems.length; i++) {
+    var zindex = Number.parseInt(
+      document.defaultView
+        .getComputedStyle(elems[i], null)
+        .getPropertyValue("z-index"),
+      10
+    );
+    if (zindex > highest) {
+      highest = zindex;
+    }
+  }
+  return highest;
+}
